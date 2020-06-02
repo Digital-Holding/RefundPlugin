7a40a007c8bfc5759441b27013733aa681019e97
matik955 <mateusz.koruna@digitalholding.pl>
2020-06-02 09:33:56 +0200

    added states changing for refund request

--- src/Controller/RefundRequestController.php
+++ src/Controller/RefundRequestController.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace Sylius\RefundPlugin\Controller;
 
+use FOS\RestBundle\View\View;
 use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
+use Sylius\Bundle\ResourceBundle\Event\ResourceControllerEvent;
+use Sylius\Component\Resource\Exception\UpdateHandlingException;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Sylius\Component\Resource\ResourceActions;
 use Sylius\RefundPlugin\Entity\RefundRequestInterface;
 use Sylius\RefundPlugin\Entity\RefundRequestMessageInterface;
 use Sylius\RefundPlugin\Event\RefundRequestMessageEvent;
@@ -13,6 +17,7 @@ use Sylius\RefundPlugin\Form\Type\RefundRequestMessageType;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 final class RefundRequestController extends ResourceController
 {
@@ -56,5 +61,50 @@ final class RefundRequestController extends ResourceController
             'form' => $form->createView(),
             'resource' => $refundRequest
         ]);
+    }
+
+    public function changeState(Request $request): Response
+    {
+        $configuration = $this->requestConfigurationFactory->create($this->metadata, $request);
+
+        $this->isGrantedOr403($configuration, ResourceActions::UPDATE);
+        $resource = $this->findOr404($configuration);
+
+        $form = $this->resourceFormFactory->create($configuration, $resource);
+        $form->handleRequest($request);
+
+        if (in_array($request->getMethod(), ['POST', 'PUT', 'PATCH'], true) && $form->isSubmitted() && $form->isValid()) {
+            $resource = $form->getData();
+
+            try {
+                $this->resourceUpdateHandler->handle($resource, $configuration, $this->manager);
+            } catch (UpdateHandlingException $exception) {
+                if (!$configuration->isHtmlRequest()) {
+                    return $this->viewHandler->handle(
+                        $configuration,
+                        View::create($form, $exception->getApiResponseCode())
+                    );
+                }
+
+                $this->flashHelper->addErrorFlash($configuration, $exception->getFlash());
+
+                return $this->redirectHandler->redirectToReferer($configuration);
+            }
+
+            if ($configuration->isHtmlRequest()) {
+                $this->flashHelper->addSuccessFlash($configuration, ResourceActions::UPDATE, $resource);
+            }
+
+            return $this->redirectHandler->redirectToResource($configuration, $resource);
+        }
+
+        if (!$configuration->isHtmlRequest()) {
+            return $this->viewHandler->handle($configuration, View::create($form, Response::HTTP_BAD_REQUEST));
+        }
+
+        $this->applyStateMachineTransitionAction($request);
+        $this->manager->flush();
+
+        return $this->redirectHandler->redirectToResource($configuration, $resource);
     }
 }
